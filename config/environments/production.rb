1d5f403ad7682d0a5ca1738c7dfeb8b8fb27356b
chenxu89 <675269196@qq.com>
2015-04-15 21:47:30 +0800

    back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,4 @@ Rails.application.configure do
   # Required for Heroku
   # Note to set this to your actual host
   config.action_mailer.default_url_options = { :host => "chenxu.herokuapp.com" }
-
 end
