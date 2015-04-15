974fa6f3c481f183d930a7cc87c0a993563bf9fc
chenxu89 <675269196@qq.com>
2015-04-15 18:56:57 +0800

    Pass Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,5 @@ Rails.application.configure do
   # Required for Heroku
   # Note to set this to your actual host
   config.action_mailer.default_url_options = { :host => "chenxu.herokuapp.com" }
+
 end
